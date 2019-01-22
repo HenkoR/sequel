4898ce1f6196280799519b02ff0ff4f2cc773a34
kyle-manganyi <tmanganyi@retrorabbit.co.za>
2019-01-22 14:26:27 +0200

    working data

--- data/createuser.sql
+++ data/createuser.sql
@@ -1,18 +1,18 @@
 USE [master]
 GO
-CREATE LOGIN [firstnamelastname] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.firstname.lastname], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+CREATE LOGIN [KyleManganyi] WITH PASSWORD=N'John2000' MUST_CHANGE, DEFAULT_DATABASE=[sql101.Kyle.Manganyi], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
 GO
-use [sql101.firstname.lastname]
+use [sql101.Kyle.Manganyi]
 
 GO
 use [master]
 
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.Kyle.Manganyi]
 GO
-CREATE USER [firstnamelastname] FOR LOGIN [firstnamelastname]
+CREATE USER [KyleManganyi] FOR LOGIN [KyleManganyi]
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.Kyle.Manganyi]
 GO
-ALTER ROLE [db_owner] ADD MEMBER [firstnamelastname]
+ALTER ROLE [db_owner] ADD MEMBER [KyleManganyi]
 GO
