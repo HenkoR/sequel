21a62182e3e2945a412225926db607f44c658f6d
Hunter27 <nmotsumi@retrorabbit.co.za>
2019-01-22 14:26:47 +0200

    First commit requested by Hanko

--- data/createuser.sql
+++ data/createuser.sql
@@ -1,18 +1,18 @@
 USE [master]
 GO
-CREATE LOGIN [firstnamelastname] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.firstname.lastname], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+CREATE LOGIN [NtokozoMotsumi] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.Ntokozo.Motsumi], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
 GO
-use [sql101.firstname.lastname]
+use [sql101.Ntokozo.Motsumi]
 
 GO
 use [master]
 
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.firstname.Motsumi]
 GO
-CREATE USER [firstnamelastname] FOR LOGIN [firstnamelastname]
+CREATE USER [NtokozoMotsumi] FOR LOGIN [NtokozoMotsumi]
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.Ntokozo.Motsumi]
 GO
-ALTER ROLE [db_owner] ADD MEMBER [firstnamelastname]
+ALTER ROLE [db_owner] ADD MEMBER [NtokozoMotsumi]
 GO
