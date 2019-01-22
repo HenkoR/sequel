7216e90e3de8bd452e257279d6c20c76a4dbccc1
ThandiTQ <ttshabalala@retrorabbit.co.za>
2019-01-22 14:27:28 +0200

    Created a user,database and added and read from the database

--- data/createuser.sql
+++ data/createuser.sql
@@ -1,18 +1,18 @@
 USE [master]
 GO
-CREATE LOGIN [firstnamelastname] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.firstname.lastname], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+CREATE LOGIN [thanditshabalala] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.thandi.tshabalala], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
 GO
-use [sql101.firstname.lastname]
+use [sql101.thandi.tshabalala]
 
 GO
 use [master]
 
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.thandi.tshabalala]
 GO
-CREATE USER [firstnamelastname] FOR LOGIN [firstnamelastname]
+CREATE USER [thanditshabalala] FOR LOGIN [thanditshabalala]
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.thandi.tshabalala]
 GO
-ALTER ROLE [db_owner] ADD MEMBER [firstnamelastname]
+ALTER ROLE [db_owner] ADD MEMBER [thanditshabalala]
 GO
