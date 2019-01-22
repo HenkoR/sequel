f7464c4c11a91f3f1e10e6639d40c9d0324736ae
Tafdav <46773345+Tafdav@users.noreply.github.com>
2019-01-22 16:12:58 +0200

    Can create my database and my user account read and write normally, still working on chache

--- src/MyTimesheet/MyTimesheet/Startup.cs
+++ src/MyTimesheet/MyTimesheet/Startup.cs
@@ -35,9 +35,11 @@ namespace MyTimesheet
                 c.SwaggerDoc("v1", new Info { Title = "My Timesheet API", Version = "v1" });
             });
 
-            var connection = @"Server=sql101labs1793591179000.westeurope.cloudapp.azure.com;Database=sql101.#NAME.SURNAME;User Id=myUsername;Password=myPassword;";
+            var connection = @"Server=sql101labs1793591179000.westeurope.cloudapp.azure.com;Database=sql101.tafara.vurayayi;User Id=tafaravurayayi;Password=retro123!#@;";
+            services.AddSingleton(Configuration);
             services.AddDbContext<TimesheetContext>
                 (options => options.UseSqlServer(connection));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
