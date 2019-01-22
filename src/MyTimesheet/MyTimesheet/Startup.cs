81fc673eeeb098fef1bc658d78581581a89d1656
KeoabetsweMorake <kmorake@retrorabbit.co.za>
2019-01-22 16:06:23 +0200

    Adding Redis code for get and post

--- src/MyTimesheet/MyTimesheet/Startup.cs
+++ src/MyTimesheet/MyTimesheet/Startup.cs
@@ -38,6 +38,7 @@ namespace MyTimesheet
             var connection = @"Server=sql101labs1793591179000.westeurope.cloudapp.azure.com;Database=sql101.keoabetswe.morake;User Id=keoabetswemorake;Password=Un@wareofl0ss;";
             services.AddDbContext<TimesheetContext>
                 (options => options.UseSqlServer(connection));
+            services.AddSingleton(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
