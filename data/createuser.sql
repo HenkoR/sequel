e4a68fc1b0e764179b5fa3139f9468a8cc538032
GiftLanga <glanga@retrorabbit.co.za>
2019-01-22 14:27:36 +0200

    Added migrations

--- data/createuser.sql
+++ data/createuser.sql
@@ -1,18 +1,18 @@
 USE [master]
 GO
-CREATE LOGIN [firstnamelastname] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.firstname.lastname], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+CREATE LOGIN [GiftLanga] WITH PASSWORD=N'G4tDB__Xcs' MUST_CHANGE, DEFAULT_DATABASE=[sql101.Gift.Langa], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
 GO
-use [sql101.firstname.lastname]
+use [sql101.Gift.Langa]
 
 GO
 use [master]
 
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.Gift.Langa]
 GO
-CREATE USER [firstnamelastname] FOR LOGIN [firstnamelastname]
+CREATE USER [GiftLanga] FOR LOGIN [GiftLanga]
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.Gift.Langa]
 GO
-ALTER ROLE [db_owner] ADD MEMBER [firstnamelastname]
+ALTER ROLE [db_owner] ADD MEMBER [GiftLanga]
 GO
