e45ef02d67b0de985e88c7d7fe4cef45b7fe7907
NombusoRR <nsibiya@retrorabbit.co.za>
2019-01-22 14:28:10 +0200

    Connecting to database, writing and reading

--- data/createuser.sql
+++ data/createuser.sql
@@ -1,18 +1,18 @@
 USE [master]
 GO
-CREATE LOGIN [firstnamelastname] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.firstname.lastname], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
+CREATE LOGIN [NombusoSibiya] WITH PASSWORD=N'rabbit123!@#' MUST_CHANGE, DEFAULT_DATABASE=[sql101.Nombuso.Sibiya], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
 GO
-use [sql101.firstname.lastname]
+use [sql101.Nombuso.Sibiya]
 
 GO
 use [master]
 
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.Nombuso.Sibiya]
 GO
-CREATE USER [firstnamelastname] FOR LOGIN [firstnamelastname]
+CREATE USER [NombusoSibiya] FOR LOGIN [NombusoSibiya]
 GO
-USE [sql101.firstname.lastname]
+USE [sql101.Nombuso.Sibiya]
 GO
-ALTER ROLE [db_owner] ADD MEMBER [firstnamelastname]
+ALTER ROLE [db_owner] ADD MEMBER [NombusoSibiya]
 GO
