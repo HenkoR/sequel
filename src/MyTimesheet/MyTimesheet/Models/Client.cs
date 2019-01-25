be8dfa1c27b1452d2d938984afed36e653f23376
KeoabetsweMorake <kmorake@retrorabbit.co.za>
2019-01-25 12:07:04 +0200

    Adding Keys and Migration

--- src/MyTimesheet/MyTimesheet/Models/Client.cs
+++ src/MyTimesheet/MyTimesheet/Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace MyTimesheet.Models
 {
     public class Client
     {
+        [Key]
         public int ClientId { get; set; }
         public string Name { get; set; }
     }
--- src/MyTimesheet/MyTimesheet/Models/TimesheetEntry.cs
+++ src/MyTimesheet/MyTimesheet/Models/TimesheetEntry.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,10 +15,13 @@ namespace MyTimesheet.Models
            John | Doe | Client X | Website | 2019-01-22 | 09:00 | 11:00 | 120 | I was rocking HTML5  | YES
            John | Doe | Client X | API | 2019-01-22 | 13:00 | 17:00 | 240 | Grafting on golang api  | YES
          */
-
+        [Key]
         public int EntryId { get; set; }
+        [ForeignKey("User")]
         public int UserId { get; set; }
+        [ForeignKey("Client")]
         public int ClientId { get; set; }
+        [ForeignKey("Project")]
         public int ProjectId { get; set; }
         public DateTime Date { get; set; }
         public DateTime TimeStart { get; set; }
@@ -24,5 +29,9 @@ namespace MyTimesheet.Models
         public int Duration { get; set; }
         public string Description { get; set; }
         public bool Billable { get; set; }
+
+        public User User { get; set; }
+        public Client client { get; set; }
+        public Project Project { get; set; }
     }
 }
