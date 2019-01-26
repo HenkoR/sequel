2d2c835ce055d302f08f6dfed96df1622eeba66c
MasulukeI <imasuluke@retrorabbit.co.za>
2019-01-26 11:35:28 +0200

    Normalised db model

---
+++ src/MyTimesheet/MyTimesheet/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTimesheet.Models
+{
+    public class Project
+    {
+        public int ProjectID { get; set; }
+        public string ProjectName { get; set; }
+        public int ClientID { get; set; }
+    }
+}
