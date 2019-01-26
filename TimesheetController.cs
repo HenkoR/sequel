743375e3257067ffc9f0e339d48ec00503f88482
kyle-manganyi <46721650+kyle-manganyi@users.noreply.github.com>
2019-01-26 21:40:05 +0200

    Add files via upload

---
+++ TimesheetController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MyTimesheet.Models;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTimesheet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimesheetController : ControllerBase
+    {
+        private readonly TimesheetContext _db;
+        readonly IConfiguration _config;
+        public TimesheetController(TimesheetContext context)
+        {
+            _db = context;
+            _config = config;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TimesheetEntry>>> Get()
+        {
+            return await _db.Entries.ToListAsync();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TimesheetEntry>> Get(int id)
+        {
+            return await _db.Entries.FindAsync(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task Post([FromBody] TimesheetEntry value)
+        {
+            await _db.Entries.AddAsync(value);
+            await _db.SaveChangesAsync();
+
+            var cacheConnection = _config.GetValue<string>("CacheConnection").ToString();
+            var lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect(cacheConnection);
+            });
+
+            IDatabase cache = lazyConnection.Value.GetDatabase();
+            await cache.StringSetAsync($"{value.Id}", value.ToString());
+
+            var cacheItem = await cache.StringGetAsync($"{value.Id}");
+
+            lazyConnection.Value.Dispose();
+
+           // return cacheItem;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] TimesheetEntry value)
+        {
+            var entry = await _db.Entries.FindAsync(id);
+            entry = value;
+            await _db.SaveChangesAsync();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            var entry = await _db.Entries.FindAsync(id);
+            _db.Entries.Remove(entry);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ TimesheetEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTimesheet.Models
+{
+    public class TimesheetEntry
+    {
+        /*
+           Name | Surname | Client | Project | Date | Time Started | Time ended | Duration | Description | Billable
+           --- | --- | --- | --- | --- | --- | --- | --- | --- | ---
+           John | Doe | Client X | Website | 2019-01-22 | 09:00 | 11:00 | 120 | I was rocking HTML5  | YES
+           John | Doe | Client X | API | 2019-01-22 | 13:00 | 17:00 | 240 | Grafting on golang api  | YES
+         */
+
+        public int Id { get; set; }
+      
+        [ForeignKey("client")]
+        public int clientID { get; set; }
+        public string Project { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime TimeStart { get; set; }
+        public DateTime TimeEnd { get; set; }
+        public int Duration { get; set; }
+        public string Description { get; set; }
+        public bool Billable { get; set; }
+    }
+}
