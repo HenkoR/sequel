3e60326fae21310e5b0abdb06a5ac47740789146
MasulukeI <imasuluke@retrorabbit.co.za>
2019-01-26 12:30:46 +0200

    Created context files

---
+++ src/MyTimesheet/MyTimesheet/Models/ProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTimesheet.Models
+{
+    public class ProjectContext: DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Project> Entries { get; set; }
+
+    }
+}
