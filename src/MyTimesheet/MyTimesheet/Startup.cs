5edcc25db380dfbdb538bb128b5c650574bfbae3
CastelloGovender <cgovender@retrorabbit.co.za>
2019-01-26 15:49:42 +0200

    Redis implementation post workshop

--- src/MyTimesheet/MyTimesheet/Startup.cs
+++ src/MyTimesheet/MyTimesheet/Startup.cs
@@ -38,6 +38,8 @@ namespace MyTimesheet
             var connection = @"Server=sql101labs1793591179000.westeurope.cloudapp.azure.com;Database=sql101.castello.govender;User Id=castellogovender;Password=P@ssword01;";
             services.AddDbContext<TimesheetContext>
                 (options => options.UseSqlServer(connection));
+            services.AddSingleton(Configuration);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
