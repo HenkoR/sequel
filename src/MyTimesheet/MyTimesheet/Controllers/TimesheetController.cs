941854d3f57d2340c224deef1fb187024c04a500
DinolanNaidoo <dnaidoo@retrorabbit.co.za>
2019-01-26 22:37:00 +0200

    Normalization Done

--- src/MyTimesheet/MyTimesheet/Controllers/TimesheetController.cs
+++ src/MyTimesheet/MyTimesheet/Controllers/TimesheetController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using MyTimesheet.Models;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +15,11 @@ namespace MyTimesheet.Controllers
     public class TimesheetController : ControllerBase
     {
         private readonly TimesheetContext _db;
-        public TimesheetController(TimesheetContext context)
+        readonly IConfiguration _config;
+        public TimesheetController(TimesheetContext context, IConfiguration config)
         {
             _db = context;
+            _config = config;
         }
 
         // GET api/values
@@ -34,10 +38,23 @@ namespace MyTimesheet.Controllers
 
         // POST api/values
         [HttpPost]
-        public async Task Post([FromBody] TimesheetEntry value)
+        public async Task<string> Post([FromBody] TimesheetEntry value)
         {
             await _db.Entries.AddAsync(value);
             await _db.SaveChangesAsync();
+
+            var cacheConnection = _config.GetValue<string>("CacheConnection").ToString();
+            var lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect(cacheConnection);
+            });
+
+            IDatabase cache = lazyConnection.Value.GetDatabase();
+            await cache.StringSetAsync($"{value.Name} - {value.Surname}", value.ToString());
+            var cacheItem = await cache.StringGetAsync($"{value.Name} - {value.Surname}");
+            lazyConnection.Value.Dispose();
+            return cacheItem;
+
         }
 
         // PUT api/values/5
---
+++ src/MyTimesheet/MyTimesheet/Migrations/20190122085349_initialMigration.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyTimesheet.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    Client = table.Column<string>(nullable: true),
+                    Project = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    TimeStart = table.Column<DateTime>(nullable: false),
+                    TimeEnd = table.Column<DateTime>(nullable: false),
+                    Duration = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Billable = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entries", x => x.Id);
+                });
+
+            migrationBuilder.Sql(
+                @"
+                UPDATE Entries
+                SET Name = Name + ' ' + Surname;");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Entries");
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Client = table.Column<string>(nullable: true),
+                    Project = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Billable = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Date",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Project = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    TimeStart = table.Column<DateTime>(nullable: false),
+                    TimeEnd = table.Column<DateTime>(nullable: false),
+                    Duration = table.Column<int>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entries", x => x.Id);
+                });
+
+            migrationBuilder.DropColumn(
+                    name: "Client",
+                    table: "Entries");
+
+            migrationBuilder.DropColumn(
+                    name: "Project",
+                    table: "Entries");
+
+            migrationBuilder.DropColumn(
+                    name: "Description",
+                    table: "Entries");
+
+            migrationBuilder.DropColumn(
+                    name: "Billable",
+                    table: "Entries");
+
+            migrationBuilder.DropColumn(
+                    name: "Date",
+                    table: "Entries");
+
+            migrationBuilder.DropColumn(
+                    name: "TimeStart",
+                    table: "Entries");
+
+            migrationBuilder.DropColumn(
+                    name: "TimeEnd",
+                    table: "Entries");
+
+            migrationBuilder.DropColumn(
+                    name: "Duration",
+                    table: "Entries");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entries");
+        }
+    }
+}
